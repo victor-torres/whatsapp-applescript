3a664e6eac9340712a8af910724a6ec73deab9b7
Sunny Pun <sunny.pun@neto.com.au>
2017-12-05 12:31:08 +0800

    Make the script works for current version of Whatsapp

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,29 +1,30 @@
-var dispatch = function(target, eventType, char) {
-    var evt = document.createEvent('TextEvent');
-    evt.initTextEvent (eventType, true, true, window, char, 0, 'en-US');
-    target.focus();
-    target.dispatchEvent(evt);
-};
+function simulateMouseEvents(element, eventName) {
+    var mouseEvent= document.createEvent('MouseEvents');
+    mouseEvent.initEvent(eventName, true, true);
+    element.dispatchEvent(mouseEvent);
+}
 
 var selectConversation = function() {
   return new Promise(function(resolve, reject) {
-    var conversation = document.querySelector('[title="' + conversation_title + '"]');
+    var conversation = jQuery('[title^="' + conversation_title + '"]')[0];
     conversation.click();
+    simulateMouseEvents( conversation , 'mousedown');
     resolve();
   });
 };
 
 var insertMessage = function() {
   return new Promise(function(resolve, reject) {
-    var textInput = document.querySelector('div.input');
-    dispatch(textInput, 'textInput', message);
+    var $element = jQuery("div.pluggable-input-body").text(message);
+    const event = new Event('input', { bubbles: true })
+    $element[0].dispatchEvent(event)
     resolve();
   });
 };
 
 var sendMessage = function() {
   return new Promise(function(resolve, reject) {
-    var sendButton = document.querySelector('button.send-container');
+    var sendButton = document.querySelector('button.compose-btn-send');
     sendButton.click();
     resolve();
   });
